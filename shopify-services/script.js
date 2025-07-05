03eb0fb98e4234d144f08c739f32f81491aa13c1
darselcool <darselcool@gmail.com>
2025-07-05 00:13:44 -0600

    15

---
+++ shopify-services/script.js
@@ -0,0 +1,14 @@
+const floatingObjects = document.querySelectorAll('.floating-objects div');
+
+floatingObjects.forEach(obj => {
+    setInterval(() => {
+        const x = Math.random() * window.innerWidth;
+        const y = Math.random() * window.innerHeight;
+        obj.style.transform = `translate(${x}px, ${y}px)`;
+    }, 5000);
+});
+
+
+
+
+
